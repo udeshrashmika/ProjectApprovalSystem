509b4276a832d4e55cd73b58ef6c5443a2ba97dc
Udesh <apurdesilva@students.nsbm.ac.lk>
2026-04-13 11:29:59 +0530

    Add project files.

---
+++ ProjectApprovalSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjectApprovalSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
