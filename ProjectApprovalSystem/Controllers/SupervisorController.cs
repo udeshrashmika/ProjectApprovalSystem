bf8962f49189c35a34e7cf8862a9265c175c216d
omiraDev <omira047@gmail.com>
2026-04-16 16:55:08 +0530

    Added Supervisor Blind Dashboard

---
+++ ProjectApprovalSystem/Controllers/SupervisorController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using ProjectApprovalSystem.Data;
+using System.Linq;
+
+namespace ProjectApprovalSystem.Controllers
+{
+     
+   
+    [Authorize]
+    public class SupervisorController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SupervisorController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult BlindDashboard()
+        {
+           
+            var proposals = _context.Proposals.ToList();
+            return View(proposals);
+        }
+    }
+}
