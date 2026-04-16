54a5a88be28a2fa12c80680c0b3ed8d075b289e7
Yasiru-I <slyasiru17@gmail.com>
2026-04-16 13:29:13 +0530

    Fixed Student Proposal form, added authorization, and configured Student role assignment

---
+++ ProjectApprovalSystem/Controllers/StudentController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BlindMatchPAS.Models;
+using ProjectApprovalSystem.Data;
+using Microsoft.AspNetCore.Authorization; 
+
+namespace ProjectApprovalSystem.Controllers
+{
+    [Authorize(Roles = "Student")]
+    public class StudentController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StudentController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(ProjectProposal proposal)
+        {
+            if (ModelState.IsValid)
+            {
+                proposal.Status = "Pending";
+
+                _context.Proposals.Add(proposal);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index", "Home");
+            }
+            return View(proposal);
+        }
+    }
+}
