3a99a8176d15d746aa812b9db8ce5078083a8207
Hashintha Nawod <hashintha@gmail.com>
2026-04-19 13:13:33 +0530

    feat: build admin oversight dashboard

---
+++ ProjectApprovalSystem/Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using ProjectApprovalSystem.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace ProjectApprovalSystem.Controllers
+{
+
+    [Authorize(Roles = "Admin")]
+    public class AdminController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AdminController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Oversight()
+        {
+
+            var proposals = await _context.Proposals.ToListAsync();
+            return View(proposals);
+        }
+    }
+}
